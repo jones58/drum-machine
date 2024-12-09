c21afa5090eabd6c44c25a27af1ced4e0c02090e
jones58 <108466184+jones58@users.noreply.github.com>
2024-12-09 15:25:37 +0000

    set up app according to user stories

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <div id="drum-machine">
+      <div id="display">
+        <button className="drum-machine">
+          Q<audio src="" className="clip" id="Q"></audio>
+        </button>
+        <button className="drum-machine">
+          W<audio src="" className="clip" id="Q"></audio>
+        </button>
+        <button className="drum-machine">
+          E<audio src="" className="clip" id="E"></audio>
+        </button>
+        <button className="drum-machine">
+          A<audio src="" className="clip" id="A"></audio>
+        </button>
+        <button className="drum-machine">
+          S<audio src="" className="clip" id="S"></audio>
+        </button>
+        <button className="drum-machine">
+          D<audio src="" className="clip" id="D"></audio>
+        </button>
+        <button className="drum-machine">
+          Z<audio src="" className="clip" id="Z"></audio>
+        </button>
+        <button className="drum-machine">
+          X<audio src="" className="clip" id="X"></audio>
+        </button>
+        <button className="drum-machine">
+          C<audio src="" className="clip" id="C"></audio>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
